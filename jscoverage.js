7f162c403639b0e5910f0a21f65d232c1518193f
siliconforks <siliconforks@6dc89bc4-a236-0410-8067-cc605125695e>
2008-06-19 15:02:53 +0000

    Adjust height of storeDiv.

--- jscoverage.js
+++ jscoverage.js
@@ -176,6 +176,11 @@ function jscoverage_setSize() {
 
   var sourceDiv = document.getElementById('sourceDiv');
   sourceDiv.style.height = (viewportHeight - jscoverage_findPos(sourceDiv) - 21) + 'px';
+
+  var storeDiv = document.getElementById('storeDiv');
+  if (storeDiv) {
+    storeDiv.style.height = (viewportHeight - jscoverage_findPos(storeDiv) - 21) + 'px';
+  }
 }
 
 /**
