e2e462ab04fca0086ebee9f5f1dce5e9e87b5846
siliconforks <siliconforks@6dc89bc4-a236-0410-8067-cc605125695e>
2008-10-06 06:35:53 +0000

    Make tests work on Windows.

--- selenium/selenium.sh
+++ selenium/selenium.sh
@@ -9,6 +9,10 @@ cleanup() {
   kill $selenium_server_pid
 }
 
+server_is_running() {
+  netstat -a -n | grep -q '\(::\|0\.0\.0\.0\):4444'
+}
+
 java -cp build Server ../doc/instrumented > server.out 2> server.err &
 server_pid=$!
 
@@ -16,15 +20,25 @@ java -jar selenium-server.jar -multiWindow > selenium.out 2> selenium.err &
 selenium_server_pid=$!
 
 # wait for the HTTP server to start
-while ! netstat -a -n --inet --inet6 | grep -q :::8000
+while ! server_is_running
 do
   sleep 0.1
 done
 
 # wait for the selenium server to start
-while ! netstat -a -n --inet --inet6 | grep -q :::4444
+while ! server_is_running
 do
   sleep 0.1
 done
 
-java -cp junit.jar:selenium-java-client-driver.jar:build junit.textui.TestRunner JSCoverageTest
+uname=`uname`
+case "$uname" in
+  CYGWIN* | MINGW*)
+    separator=';'
+    ;;
+  *)
+    separator=':'
+    ;;
+esac
+
+java -cp "junit.jar${separator}selenium-java-client-driver.jar${separator}build" junit.textui.TestRunner JSCoverageTest
