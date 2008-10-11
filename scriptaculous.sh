30fd762bf938eb71dd6dae1c3ef43ef3b115f5db
siliconforks <siliconforks@6dc89bc4-a236-0410-8067-cc605125695e>
2008-10-11 22:39:56 +0000

    Add report.js.

--- scriptaculous.sh
+++ scriptaculous.sh
@@ -2,7 +2,7 @@
 
 rm -fr scriptaculous scriptaculous-instrumented
 mkdir -p scriptaculous/jscoverage
-cp -a jscoverage.* jscoverage-throbber.gif scriptaculous/jscoverage
+cp -a jscoverage.* report.js jscoverage-throbber.gif scriptaculous/jscoverage
 ./jscoverage scriptaculous scriptaculous-instrumented
 cp -a scriptaculous.js scriptaculous.html scriptaculous-data.html scriptaculous-instrumented/jscoverage
 cp -a scriptaculous-js-1.8.1 scriptaculous-instrumented/jscoverage
