92c2f69a9b7c04f34ba26240ecfcf37132cd2f21
siliconforks <siliconforks@6dc89bc4-a236-0410-8067-cc605125695e>
2010-04-01 23:15:10 +0000

    Catch exception that may be thrown when accessing top or top.opener.

--- header.js
+++ header.js
@@ -1,29 +1,38 @@
-if (typeof top === 'object' && top !== null) {
-  if (typeof top.opener === 'object' && top.opener !== null) {
+try {
+  if (typeof top === 'object' && top !== null && typeof top.opener === 'object' && top.opener !== null) {
     // this is a browser window that was opened from another window
-    if (typeof top.opener._$jscoverage === 'object' && top.opener._$jscoverage !== null) {
-      var _$jscoverage = top._$jscoverage = top.opener._$jscoverage;
-    }
-    else {
-      var _$jscoverage = top._$jscoverage = top.opener._$jscoverage = {};
-    }
-  }
-  else {
-    // this is a browser window
-    if (typeof top._$jscoverage === 'object' && top._$jscoverage !== null) {
-      var _$jscoverage = top._$jscoverage;
-    }
-    else {
-      var _$jscoverage = top._$jscoverage = {};
+
+    if (! top.opener._$jscoverage) {
+      top.opener._$jscoverage = {};
     }
   }
 }
-else {
-  // no "top" - this is not a browser window
-  if (typeof _$jscoverage === 'object' && _$jscoverage !== null) {
-    // nothing to do
+catch (e) {}
+
+try {
+  if (typeof top === 'object' && top !== null) {
+    // this is a browser window
+
+    try {
+      if (typeof top.opener === 'object' && top.opener !== null && top.opener._$jscoverage) {
+        top._$jscoverage = top.opener._$jscoverage;
+      }
+    }
+    catch (e) {}
+
+    if (! top._$jscoverage) {
+      top._$jscoverage = {};
+    }
   }
-  else {
-    var _$jscoverage = {};
+}
+catch (e) {}
+
+try {
+  if (typeof top === 'object' && top !== null && top._$jscoverage) {
+    this._$jscoverage = top._$jscoverage;
   }
+}
+catch (e) {}
+if (! this._$jscoverage) {
+  this._$jscoverage = {};
 }
